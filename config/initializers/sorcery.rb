16150d146234473b2360ba05386d93a1564065a8
takeshi-hashimoto0607 <b8011079t@gmail.com>
2026-05-07 17:44:30 +0900

    fix: Rubocopの指摘に従い配列内のスペースを追加

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -2,7 +2,7 @@ Rails.application.config.sorcery.submodules = []
 
 Rails.application.config.sorcery.configure do |config|
   config.user_config do |user|
-    user.username_attribute_names = [:name]
+    user.username_attribute_names = [ :name ]
     user.password_attribute_name = :password
     user.crypted_password_attribute_name = :crypted_password
     user.salt_attribute_name = :salt
